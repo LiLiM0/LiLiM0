11103212d8cb9dd1d66d9284e8ed1b35174135d5
LiLiM0 <lilim0@126.com>
2021-04-01 18:17:50 +0800

    1.增加dto层，用于controller和service层 2.增加ChapterDto 3.修改ChapterService，将返回Chapter改为返回ChapterDto

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -7,6 +7,8 @@ create table `chapter`(
 primary key (`id`)
 )engine = innodb default charset=utf8mb4 comment = '大章';
 
+insert into `chapter` (id, course_id, name) values ('00000000','00000000','测试大章一');
+insert into `chapter` (id, course_id, name) values ('00000001','00000000','测试大章二');
 
 ---------测试
 drop table if exists `test`;
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -2,10 +2,13 @@ package com.course.server.service;
 
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
+import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,13 +17,21 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<Chapter> list(){
+    public List<ChapterDto> list() {
         //Mybatis自动化curd语句
         ChapterExample chapterExample = new ChapterExample();
-        chapterExample.createCriteria().andIdEqualTo("1");
+        //chapterExample.createCriteria().andIdEqualTo("1");
         //设置查询条件从小到大
-        chapterExample.setOrderByClause("id desc");
-        return chapterMapper.selectByExample(chapterExample);
+        //chapterExample.setOrderByClause("id desc");
+        List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
+        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
+        for (int i = 0, l = chapterList.size(); i < l; i++) {
+            Chapter chapter = chapterList.get(i);
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter, chapterDto);
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
     }
 
 }
