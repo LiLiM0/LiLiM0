44e45ec2b93fb294d38cd6b7feacf6153252c85c
LiLiM0 <lilim0@126.com>
2021-04-02 01:03:55 +0800

    1.分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的重要参数要加@RequestBody注解

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,8 +3,10 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +20,10 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list() {
+    public void list(PageDto pageDto) {
 
         //对往下第一个遇到的select语句进行分页
-        PageHelper.startPage(2,1);
+        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
 
         //Mybatis自动化curd语句
         ChapterExample chapterExample = new ChapterExample();
@@ -29,8 +31,11 @@ public class ChapterService {
         //chapterExample.createCriteria().andIdEqualTo("1");
         //设置查询条件从小到大
         //chapterExample.setOrderByClause("id desc");
-
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
+
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        pageDto.setTotal(pageInfo.getTotal());
+
         List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
         for (int i = 0, l = chapterList.size(); i < l; i++) {
             Chapter chapter = chapterList.get(i);
@@ -38,7 +43,7 @@ public class ChapterService {
             BeanUtils.copyProperties(chapter, chapterDto);
             chapterDtoList.add(chapterDto);
         }
-        return chapterDtoList;
+        pageDto.setList(chapterDtoList);
     }
 
 }
