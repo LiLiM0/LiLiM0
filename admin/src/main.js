33cf6cf15fea4a7929bd7ad7de8bafe014d07837
LiLiM0 <lilim0@126.com>
2021-04-01 23:25:39 +0800

    1.集成axios完成前后端交互 2.增加CorsConfig，解决前后端跨域的问题

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import axios from "axios";
 
 Vue.config.productionTip = false
+//Vue.prototype.XXXX Vue组件的全局变量 可以在任意Vue组件中用this.xxx来获取这个值
+Vue.prototype.$ajax = axios;
 
 new Vue({
     router,
     render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
---
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//CORS,Cross-Origin Resource Sharing跨站点资源分享，属于跨域问题。同个IP的不同端口间的访问也属于跨域。前后端分离必然有跨域问题
+//解决跨域问题，增加CorsConfig.java
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
