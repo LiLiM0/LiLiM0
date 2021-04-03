5c985002c291a5f7460377ca7f93824198f42456
LiLiM0 <lilim0@126.com>
2021-04-04 01:31:57 +0800

    1.制作Loading组件，内部用jquery blockui插件实现

--- admin/public/static/js/toast.js
+++ admin/public/static/js/toast.js
@@ -1,31 +1,37 @@
-Toast = {
-  success: function (message) {
-    Swal.fire({
-      position: 'top-end',
-      icon: 'success',
-      title: message,
-      showConfirmButton: false,
-      timer: 3000
-    })
-  },
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
 
-  error: function (message) {
-    Swal.fire({
-      position: 'top-end',
-      icon: 'error',
-      title: message,
-      showConfirmButton: false,
-      timer: 3000
-    })
-  },
+toast = {
+    success: function (message) {
+        Toast.fire({
+            // position: 'top-end',
+            icon: 'success',
+            title: message,
+        })
+    },
 
-  warning: function (message) {
-    Swal.fire({
-      position: 'top-end',
-      icon: 'warning',
-      title: message,
-      showConfirmButton: false,
-      timer: 3000
-    })
-  }
+    error: function (message) {
+        Toast.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: message,
+        })
+    },
+
+    warning: function (message) {
+        Toast.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: message,
+        })
+    }
 };
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -149,11 +149,13 @@
             },
             list(page) {
                 let _this = this;
+                Loading.show();
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
                     page: page,
                     size: _this.$refs.pagination.size,
                 }).then((response) => {
                     console.log("查询大章列表结果：", response);
+                    Loading.hide();
                     let resp = response.data;
                     _this.chapters = resp.content.list;
                     _this.$refs.pagination.render(page, resp.content.total);
@@ -162,13 +164,16 @@
 
             save(page) {
                 let _this = this;
+                Loading.show();
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
                     _this.chapter).then((response) => {
+                    Loading.hide();
                     console.log("保存大章列表结果：", response);
                     let resp = response.data;
                     if (resp.success) {
                         $("#form-modal").modal("hide");
                         _this.list(1);
+                        toast.success("保存成功！")
                     }
                 })
             },
@@ -185,16 +190,14 @@
                 })
                     .then((result) => {
                         if (result.value) {
+                            Loading.show();
                             _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
+                                Loading.hide();
                                 console.log("删除大章列表结果：", response);
                                 let resp = response.data;
                                 if (resp.success) {
                                     _this.list(1);
-                                    Swal.fire(
-                                        '数据已成功删除',
-                                        '>_<',
-                                        'success'
-                                    );
+                                    toast.success("删除成功")
                                 }
                             })
                         }
