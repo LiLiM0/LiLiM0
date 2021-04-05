e420032652298240d225e75b779cdffcc216962c
LiLiM0 <lilim0@126.com>
2021-04-05 22:00:08 +0800

    1.增加vue多环境配置，后端地址改为读环境变量

--- admin/src/main.js
+++ admin/src/main.js
@@ -7,7 +7,28 @@ Vue.config.productionTip = false
 //Vue.prototype.XXXX Vue组件的全局变量 可以在任意Vue组件中用this.xxx来获取这个值
 Vue.prototype.$ajax = axios;
 
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log("请求：", config);
+    // let token = Tool.getLoginUser().token;
+    // if (Tool.isNotEmpty(token)) {
+    //     config.headers.token = token;
+    //     console.log("请求headers增加token:", token);
+    // }
+    return config;
+}, error => {
+});
+axios.interceptors.response.use(function (response) {
+    console.log("返回结果：", response);
+    return response;
+}, error => {
+});
+
 new Vue({
     router,
     render: h => h(App),
 }).$mount('#app');
+
+console.log("环境：", process.env.NODE_ENV);
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -150,7 +150,7 @@
             list(page) {
                 let _this = this;
                 Loading.show();
-                _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
+                _this.$ajax.post(process.env.VUE_APP_SERVER+'/business/admin/chapter/list', {
                     page: page,
                     size: _this.$refs.pagination.size,
                 }).then((response) => {
@@ -174,7 +174,7 @@
 
                 Loading.show();
 
-                _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
+                _this.$ajax.post(process.env.VUE_APP_SERVER+'/business/admin/chapter/save',
                     _this.chapter).then((response) => {
                     Loading.hide();
                     console.log("保存大章列表结果：", response);
@@ -192,7 +192,7 @@
                 let _this = this;
                 Confirm.show(">_<", function () {
                     Loading.show();
-                    _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
+                    _this.$ajax.delete(process.env.VUE_APP_SERVER+'/business/admin/chapter/delete/' + id).then((response) => {
                         Loading.hide();
                         console.log("删除大章列表结果：", response);
                         let resp = response.data;
