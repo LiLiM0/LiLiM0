bb7bfc2827e7a99937be374a3b3d33655a1e9cf9
LiLiM0 <lilim0@126.com>
2021-04-08 10:03:55 +0800

    1.课程内容表设计与基本代码生成

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -131,6 +131,14 @@ create table `course_category` (
                                    primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='课程分类';
 
+-- 课程内容
+drop table if exists `course_content`;
+create table `course_content` (
+  `id` char(8) not null default '' comment '课程id',
+  `content` mediumtext not null comment '课程内容',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='课程内容';
+
 # 测试
 drop table if exists `test`;
 create table `test`(
