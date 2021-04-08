3b1bd3d97d9e96555507f393a6f1693b6d39a709
LiLiM0 <lilim0@126.com>
2021-04-08 18:06:08 +0800

    1.菜单隐藏掉大章和小节

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -116,7 +116,7 @@
             _this.course = course;
             _this.list(1);
             // sidebar激活样式方法一
-            // this.$parent.activeSidebar("business-chapter-sidebar");
+            this.$parent.activeSidebar("business-course-sidebar");
         },
         methods: {
             add() {
