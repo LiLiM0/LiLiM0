35157b378e8484ade48e91ff789fcac60d54616a
LiLiM0 <lilim0@126.com>
2021-04-08 21:33:32 +0800

    1.讲师界面布局优化

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -14,88 +14,99 @@
 
         <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
-        <table id="simple-table" class="table  table-bordered table-hover">
-            <thead>
-            <tr>
-                                        <th>id</th>
-                        <th>姓名</th>
-                        <th>昵称</th>
-                        <th>头像</th>
-                        <th>职位</th>
-                        <th>座右铭</th>
-                        <th>简介</th>
-                <th>操作</th>
-            </tr>
-            </thead>
+        <div class="row">
+            <div v-for="teacher in teachers" class="col-md-3">
+                <div>
+          <span class="profile-picture">
+            <img v-show="!teacher.image" class="editable img-responsive editable-click editable-empty"
+                 src="/static/image/讲师头像/头像1.jpg" v-bind:title="teacher.intro"/>
+            <img v-show="teacher.image" class="editable img-responsive editable-click editable-empty"
+                 v-bind:src="teacher.image" v-bind:title="teacher.intro"/>
+          </span>
 
-            <tbody>
-            <tr v-for="teacher in teachers">
-                            <td>{{teacher.id}}</td>
-                            <td>{{teacher.name}}</td>
-                            <td>{{teacher.nickname}}</td>
-                            <td>{{teacher.image}}</td>
-                            <td>{{teacher.position}}</td>
-                            <td>{{teacher.motto}}</td>
-                            <td>{{teacher.intro}}</td>
-                <td>
-                    <div class="hidden-sm hidden-xs btn-group">
-                        <button @click="edit(teacher)" class="btn btn-xs btn-info">
-                            <i class="ace-icon fa fa-pencil bigger-120"></i>
-                        </button>
-                        <button @click="del(teacher.id)" class="btn btn-xs btn-danger">
-                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                        </button>
+                    <div class="space-4"></div>
+
+                    <div class="width-85 label label-info label-xlg arrowed-in arrowed-in-right">
+                        <div class="inline position-relative">
+                            <a href="javascript:;" class="user-title-label dropdown-toggle" data-toggle="dropdown">
+                                <i class="ace-icon fa fa-circle light-green"></i>
+                                &nbsp;
+                                <span class="white">{{teacher.position}}</span>
+                            </a>
+                        </div>
                     </div>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+                </div>
+                <div class="space-6"></div>
+
+                <div class="text-center">
+                    <a href="javascript:;" class="text-info bigger-110" v-bind:title="teacher.motto">
+                        <i class="ace-icon fa fa-user"></i>
+                        {{teacher.name}}【{{teacher.nickname}}】
+                    </a>
+                </div>
+
+                <div class="space-6"></div>
+
+                <div class="profile-social-links align-center">
+                    <button v-on:click="edit(teacher)" class="btn btn-xs btn-info">
+                        <i class="ace-icon fa fa-pencil bigger-120"></i>
+                    </button>
+                    &nbsp;
+                    <button v-on:click="del(teacher.id)" class="btn btn-xs btn-danger">
+                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                    </button>
+                </div>
+
+                <div class="hr hr16 dotted"></div>
+            </div>
+        </div>
 
         <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
                         <h4 class="modal-title">表单</h4>
                     </div>
                     <div class="modal-body">
                         <form class="form-horizontal">
-                                        <div class="form-group">
-                                            <label class="col-sm-2 control-label">姓名</label>
-                                            <div class="col-sm-10">
-                                                <input v-model="teacher.name" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-2 control-label">昵称</label>
-                                            <div class="col-sm-10">
-                                                <input v-model="teacher.nickname" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-2 control-label">头像</label>
-                                            <div class="col-sm-10">
-                                                <input v-model="teacher.image" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-2 control-label">职位</label>
-                                            <div class="col-sm-10">
-                                                <input v-model="teacher.position" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-2 control-label">座右铭</label>
-                                            <div class="col-sm-10">
-                                                <input v-model="teacher.motto" class="form-control">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-2 control-label">简介</label>
-                                            <div class="col-sm-10">
-                                                <input v-model="teacher.intro" class="form-control">
-                                            </div>
-                                        </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">姓名</label>
+                                <div class="col-sm-10">
+                                    <input v-model="teacher.name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">昵称</label>
+                                <div class="col-sm-10">
+                                    <input v-model="teacher.nickname" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">头像</label>
+                                <div class="col-sm-10">
+                                    <input v-model="teacher.image" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">职位</label>
+                                <div class="col-sm-10">
+                                    <input v-model="teacher.position" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">座右铭</label>
+                                <div class="col-sm-10">
+                                    <input v-model="teacher.motto" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">简介</label>
+                                <div class="col-sm-10">
+                                    <textarea v-model="teacher.intro" class="form-control" rows="5"></textarea>
+                                </div>
+                            </div>
                         </form>
                     </div>
                     <div class="modal-footer">
@@ -110,16 +121,17 @@
 
 <script>
     import Pagination from "../../components/pagination";
+
     export default {
         components: {Pagination},
         name: "business-teacher",
-        data: function() {
+        data: function () {
             return {
-            teacher: {},
-            teachers: [],
-        }
+                teacher: {},
+                teachers: [],
+            }
         },
-        mounted: function() {
+        mounted: function () {
             let _this = this;
             _this.$refs.pagination.size = 5;
             _this.list(1);
@@ -155,7 +167,7 @@
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/list', {
                     page: page,
                     size: _this.$refs.pagination.size,
-                }).then((response)=>{
+                }).then((response) => {
                     Loading.hide();
                     let resp = response.data;
                     _this.teachers = resp.content.list;
@@ -184,7 +196,7 @@
                 }
 
                 Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/save', _this.teacher).then((response)=>{
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/save', _this.teacher).then((response) => {
                     Loading.hide();
                     let resp = response.data;
                     if (resp.success) {
@@ -204,7 +216,7 @@
                 let _this = this;
                 Confirm.show("删除讲师后不可恢复，确认删除？", function () {
                     Loading.show();
-                    _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/teacher/delete/' + id).then((response)=>{
+                    _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/teacher/delete/' + id).then((response) => {
                         Loading.hide();
                         let resp = response.data;
                         if (resp.success) {
