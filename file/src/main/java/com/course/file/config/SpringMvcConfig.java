0cf9556d802a18db2f845bcfb9e9036444e2d810
LiLiM0 <lilim0@126.com>
2021-04-09 12:18:07 +0800

    1.文件上传功能开发：增加springboot静态资源配置，配置文件访问路径

--- file/src/main/java/com/course/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/course/file/config/SpringMvcConfig.java
@@ -1,6 +1,7 @@
 package com.course.file.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -8,9 +9,10 @@ public class SpringMvcConfig implements WebMvcConfigurer {
 
 //    @Value("${file.path}")
 //    private String FILE_PATH;
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
-//    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/Project/file/course/");
+        //http://127.0.0.1:9003/file/f/teacher/IWacNRVX-avatar4.png
+    }
 }
