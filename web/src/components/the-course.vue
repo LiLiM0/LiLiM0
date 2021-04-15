948295343cbb0dba7cc85ca50d0770fae47d04a8
LiLiN <lilim0@126.com>
2021-04-15 15:52:43 +0800

    1.课程列表页面开发：增加课程详情页面和路由

--- web/src/components/the-course.vue
+++ web/src/components/the-course.vue
@@ -6,7 +6,7 @@
       <p class="card-text">{{course.summary}}</p>
       <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">课程详情</button>
+          <router-link v-bind:to="'/detail?id=' + course.id" class="btn btn-outline-secondary">课程详情</router-link>
         </div>
         <div class="text-muted">
           <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{course.price}}</span>&nbsp;
