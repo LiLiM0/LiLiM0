cc98c400301fd8f839be3662eb4cb96535ec427a
LiLiN <lilim0@126.com>
2021-04-15 11:37:57 +0800

    1.课程列表开发：前端分页，增加分页组件

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -1,24 +1,30 @@
 <template>
     <main role="main">
-        <div class="album py-5 bg-light">
-            <div class="container">
-                <div class="row">
-                    <div v-for="o in courses" class="col-md-4">
-                        <the-course v-bind:course="o"></the-course>
-                    </div>
-                    <h3 v-show="courses.length === 0">课程还未上架</h3>
-                </div>
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <pagination ref="pagination" v-bind:list="listCourse"></pagination>
             </div>
+          </div>
+          <br>
+          <div class="row">
+            <div v-for="o in courses" class="col-md-4">
+              <the-course v-bind:course="o"></the-course>
+            </div>
+            <h3 v-show="courses.length === 0">课程还未上架</h3>
+          </div>
         </div>
-
+      </div>
     </main>
 </template>
 
 <script>
     import TheCourse from "../components/the-course";
+    import Pagination from "../components/pagination";
 
     export default {
-        components: {TheCourse},
+        components: {Pagination,TheCourse},
         name: 'list',
         data: function () {
             return {
@@ -27,6 +33,7 @@
         },
         mounted() {
             let _this = this;
+          _this.$refs.pagination.size = 2;
             _this.listCourse();
         },
         methods: {
@@ -37,11 +44,12 @@
             let _this = this;
             _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list', {
               page: page,
-              size: 3,
+              size: _this.$refs.pagination.size,
             }).then((response) => {
               let resp = response.data;
               if (resp.success) {
                 _this.courses = resp.content.list;
+                _this.$refs.pagination.render(page, resp.content.total);
               }
             }).catch((response) => {
               console.log("error：", response);
