b3c2002eede0cae884a7abd7536495619457aceb
LiLiN <lilim0@126.com>
2021-04-15 15:01:00 +0800

    1.课程列表页面开发，点击一级分类时显示激活状态，并显示对应的二级分类，如果点击的是【全部】，则显示所有的二级分类 2.课程列表开发：点击二级分类时显示激活状态

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -57,6 +57,7 @@
                 courses: [],
               level1: [],
               level2: [],
+              categorys: [],
             }
         },
         mounted() {
@@ -93,6 +94,7 @@
             _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/category/all').then((response) => {
               let resp = response.data;
               let categorys = resp.content;
+              _this.categorys = categorys;
 
               // 将所有记录格式化成树形结构
               _this.level1 = [];
@@ -100,15 +102,6 @@
                 let c = categorys[i];
                 if (c.parent === '00000000') {
                   _this.level1.push(c);
-                  for (let j = 0; j < categorys.length; j++) {
-                    let child = categorys[j];
-                    if (child.parent === c.id) {
-                      if (Tool.isEmpty(c.children)) {
-                        c.children = [];
-                      }
-                      c.children.push(child);
-                    }
-                  }
                 } else {
                   _this.level2.push(c);
                 }
@@ -117,11 +110,50 @@
           },
 
           /**
+           * 点击一级分类时
+           * @param level1Id
+           */
+          onClickLevel1(level1Id) {
+            let _this = this;
+
+            //点击一级分类时，显示激活状态
+            $("#category-" + level1Id).siblings("a").removeClass("cur");
+            $("#category-" + level1Id).addClass("cur");
+            //点击一级分类时，二级分类【无限】按钮要设置激活状态
+            $("#category-11111111").siblings("a").removeClass("on");
+            $("#category-11111111").addClass("on");
+
+            // 注意：要先把level2中所有的值清空，再往里放
+            _this.level2 = [];
+            let categorys = _this.categorys;
+            // 如果点击的是【全部】，则显示所有的二级分类
+            if (level1Id === '00000000') {
+              for (let i = 0; i < categorys.length; i++) {
+                let c = categorys[i];
+                if (c.parent !== "00000000") {
+                  _this.level2.push(c);
+                }
+              }
+            }
+            // 如果点击的是某个一级分类，则显示该一级分类下的二级分类
+            if (level1Id !== '00000000') {
+              for (let i = 0; i < categorys.length; i++) {
+                let c = categorys[i];
+                if (c.parent === level1Id) {
+                  _this.level2.push(c);
+                }
+              }
+            }
+          },
+
+          /**
            * 点击二级分类时
            * @param level1Id
            */
           onClickLevel2(level2Id) {
             let _this = this;
+            $("#category-" + level2Id).siblings("a").removeClass("on");
+            $("#category-" + level2Id).addClass("on");
           },
 
         }
