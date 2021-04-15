ff028924990fbdbad2a3c7f5c33f58f2f64d6a55
LiLiN <lilim0@126.com>
2021-04-16 02:44:39 +0800

    1.增加会员管理页面：新增member表，在之前代码生成器的步骤后面，加上以下步骤：      1) 在resource.json中增加新资源      2) 在【资源管理】页面，配置最新的resource.json      3) 在【角色管理】页面，为相应的角色配置新的资源      4) 重新登录控台

--- admin/src/router.js
+++ admin/src/router.js
@@ -13,6 +13,7 @@ import Content from "./views/admin/content.vue"
 import User from "./views/admin/user.vue"
 import Resource from "./views/admin/resource.vue"
 import Role from "./views/admin/role.vue"
+import Member from "./views/admin/member.vue"
 
 
 Vue.use(Router);
@@ -83,6 +84,10 @@ export default new Router({
             path: "system/role",
             name: "system/role",
             component: Role
+        }, {
+            path: "business/member",
+            name: "business/member",
+            component: Member
         }]
     }]
 })
