5587119de159879516bd274d554b2802607faa96
LiLiN <lilim0@126.com>
2021-04-16 18:54:22 +0800

    1.会员登录注册功能开发:增加发送短信后倒计时功能,可修改countdown参数进行调试

--- web/src/components/login.vue
+++ web/src/components/login.vue
@@ -265,26 +265,49 @@
           use: SMS_USE.REGISTER.key
         };
 
-        _this.sendSmsCode(sms);
+        _this.sendSmsCode(sms, "register-send-code-btn");
       },
 
       /**
        * 发送短信
        */
-      sendSmsCode(sms) {
+      sendSmsCode(sms, btnId) {
         let _this = this;
 
         // 调服务端发短信接口
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/sms/send', sms).then((res)=> {
           let response = res.data;
           if (response.success) {
-            Toast.success("短信已发送")
+            Toast.success("短信已发送");
+            // 开始倒计时
+            _this.countdown = 60;
+            _this.setTime(btnId);
           } else {
             Toast.warning(response.message);
           }
         })
       },
 
+      /**
+       * 倒计时
+       * @param btnId
+       */
+      setTime(btnId) {
+        let _this = this;
+        let btn = $("#" + btnId);
+        if (_this.countdown === 0) {
+          btn.removeAttr("disabled");
+          btn.text("获取验证码");
+          return;
+        } else {
+          btn.attr("disabled", true);
+          btn.text("重新发送(" + _this.countdown + ")");
+          _this.countdown--;
+        }
+        setTimeout(function () {
+          _this.setTime(btnId);
+        }, 1000);
+      },
     }
   }
 </script>
