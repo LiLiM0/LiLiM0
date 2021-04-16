d459e851bb2d45f91f9322ac0c1573ca626ff9dc
LiLiN <lilim0@126.com>
2021-04-16 14:42:50 +0800

    1.会员登录注册功能开发:登录成功后，头部导航显示会员昵称

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -29,7 +29,7 @@
               </div>
             </li>
           </ul>
-          <span class="text-white">欢迎：</span>
+          <span v-show="loginMember.id" class="text-white pr-3">您好：{{loginMember.name}}</span>
           <button v-on:click="openLoginModal()" class="btn btn-outline-light my-2 my-sm-0" type="submit">登录/注册</button>
         </div>
       </div>
@@ -43,6 +43,15 @@ import TheLogin from "./login";
   export default {
     name: 'theHeader',
     components: {TheLogin},
+    data: function () {
+      return {
+        loginMember: {}
+      }
+    },
+    mounted() {
+      let _this = this;
+      _this.loginMember = Tool.getLoginMember();
+    },
     methods: {
       /**
        * 打开登录注册窗口
@@ -50,6 +59,11 @@ import TheLogin from "./login";
       openLoginModal() {
         let _this = this;
         _this.$refs.loginComponent.openLoginModal();
+      },
+
+      setLoginMember(loginMember) {
+        let _this = this;
+        _this.loginMember = loginMember;
       },
 
     }
